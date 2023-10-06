e5f99fc3f1bcb48b5fb876a0bee0bb5c26ef544b
Nvutu-Eladio <Eladio2019@hotmail.com>
2023-10-05 21:44:47 -0300

    atualizando o projeto

---
+++ src/componentes/CampoTexto/index.js
@@ -0,0 +1,28 @@
+import './CampoTexto.css'
+
+const CampoTexto = (props) => {
+
+    const placeholderModificada = `${props.placeholder}...`
+
+    // let valor = 'Nvutu Eladio'
+
+    // const [valor, setValor] = useState('')
+
+    const aoDigitado = (evento) => {
+        // setValor(evento.target.value)
+        // console.log(valor)
+        props.aoAlterado(evento.target.value)
+    }
+
+
+    return (
+            <div className="campo-texto">
+                <label>
+                    {props.label}
+                </label>
+                <input value={props.valor} onChange={aoDigitado} required={props.obrigatorio} placeholder= {placeholderModificada} />
+            </div>
+    )
+}
+
+export default CampoTexto
---
+++ src/componentes/ListaSuspensa/index.js
@@ -0,0 +1,17 @@
+import './ListaSuspensa.css'
+
+const ListaSuspensa = (props) => {
+    return (
+        <div className='lista-suspensa'>
+           <label>{props.label}</label> 
+           <select onChange={evento => props.aoAlterado(evento.target.value)} required={props.required} value={props.value}>
+            {/* Para cada item retorna uma option */}
+            {/* props.itens.map(item =>{ return <option>{item}</option>}) */}
+            {/* Cada option deve ter uma chave key, para poder identificar no momento da renderizacao */}
+                {props.itens.map(item => <option key={item}>{item}</option>)}
+           </select>
+        </div>
+    )
+}
+
+export default ListaSuspensa
